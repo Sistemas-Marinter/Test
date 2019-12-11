3726387cc859fee1e9ff7accb350e05603995550
jiramh <58784222+jiramh@users.noreply.github.com>
2019-12-11 12:13:28 -0600

    Cambio texto button

--- TestGithub/TestGithub/Form1.vb
+++ TestGithub/TestGithub/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("HolaMundo")
+        MessageBox.Show("Hola Mundo test")
     End Sub
 End Class
